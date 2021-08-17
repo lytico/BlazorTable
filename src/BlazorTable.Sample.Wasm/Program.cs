55e3bda6b13c35d3dc47e498d261f764009b8311
lytico <lytico@users.sourceforge.net>
2021-08-17 14:00:14 +0200

    upgrade to net5

--- src/BlazorTable.Sample.Wasm/Program.cs
+++ src/BlazorTable.Sample.Wasm/Program.cs
@@ -1,20 +1,22 @@
-﻿using BlazorTable.Sample.Shared;
+﻿using System;
+using System.Net.Http;
+using BlazorTable.Sample.Shared;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace BlazorTable.Sample.Wasm
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
+namespace BlazorTable.Sample.Wasm {
+
+    public class Program {
+
+        public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
-
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
+
     }
+
 }
